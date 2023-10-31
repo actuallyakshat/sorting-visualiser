fd627931d845bc97670b700b6269540105f3731b
actuallyakshat <akshatdubey0808@gmail.com>
2023-10-31 15:39:05 +0530

    fixed bubble sort

--- scripts/bubble.js
+++ scripts/bubble.js
@@ -1,44 +1,33 @@
-async function bubble(){
-  console.log('In Bubble()');
+async function bubbleSort() {
   const ele = document.querySelectorAll('.bar');
-  for(let i=0; i<ele.length-1; ++i){
-      console.log(`In ${i}th loop`);
-      for(let j=0; j<ele.length-i-1;++j){
-      console.log('In jth loop');
-      ele[j].style.background= 'rgb(242, 255, 3)';                        //yellow
-      ele[j+1].style.background= 'rgb(242, 255, 3)';                      //yellow
-      await waitforme(delay+60);
+  const n = ele.length;
+  
+  for (let i = 0; i < n - 1; ++i) {
+    for (let j = 0; j < n - i - 1; ++j) {
+      ele[j].style.background = 'red';
+      ele[j + 1].style.background = 'red';
 
+      await waitforme(animationSpeed);
 
-      if(parseInt(ele[j].style.height) > parseInt(ele[j+1].style.height)){
-          console.log('In if condition');
-          swap(ele[j], ele[j+1]);
+      const height1 = parseInt(ele[j].style.height);
+      const height2 = parseInt(ele[j + 1].style.height);
+
+      if (height1 > height2) {
+        swap(ele[j], ele[j + 1]);
       }
-      ele[j].style.background = 'rgb(0, 255, 255)';                          //cyan
-      ele[j+1].style.background = 'rgb(0, 255, 255)';                        //cyan
-      }
-      ele[ele.length-i-1].style.background = 'rgb(5, 250, 30)';               //lightgreen
+
+      ele[j].style.background = 'rgb(255, 255, 255)';
+      ele[j + 1].style.background = 'rgb(255, 255, 255)';
+    }
+
+    ele[n - i - 1].style.background = 'rgb(5, 250, 30)';
   }
-  ele[0].style.background = 'rgb(5, 250, 30)';                                //lightgreen
+  ele[0].style.background = 'rgb(5, 250, 30)';
 }
 
 const bubbleBtn = document.querySelector('.bubble');
-bubbleBtn.addEventListener('click', async function(){
-    disableButtons();
-    await bubble();
-    enableButtons();
+bubbleBtn.addEventListener('click', async function() {
+  disableButtons();
+  await bubbleSort();
+  enableButtons();
 });
-
-function updateBubbleTime() {
-  let data = document.getElementsByClassName("time-details");
-  data[0].innerHTML = "O(N)";
-  data[1].innerHTML = "O(N<sup>2</sup>)";
-  data[2].innerHTML = "O(N<sup>2</sup>)";
-}
-
-function updateBubbleSpace() {
-  let data = document.getElementsByClassName("space-details");
-  data[0].innerHTML = "O(1)";
-  data[1].innerHTML = "O(1)";
-  data[2].innerHTML = "O(1)";
-}
--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 const sortingButtons = document.querySelectorAll(".btn"); //fetching to disable and enable all buttons
 let arr = []; //we will work on this array
 let determineSpeed = document.querySelector(".speed-slider").value; //value from speed slider
-let determineSize = document.querySelector(".size-slider").value; //value from size slider
+let arraySize = document.querySelector(".size-slider").value; //value from size slider
 const sortingWindow = document.querySelector(".sorting-window");
 
 generateArray();
@@ -10,45 +10,43 @@ updateSpeed();
 //changing sliders automatically calls the updation functions
 document
   .querySelector(".size-slider")
-  .addEventListener("input", updateArraySize);
+  .addEventListener("input", generateArray);
 document.querySelector(".speed-slider").addEventListener("input", updateSpeed);
 
 //swap function
 function swap(ele1, ele2) {
-  console.log("In swap()");
-
   let temp = ele1.style.height;
   ele1.style.height = ele2.style.height;
   ele2.style.height = temp;
+}
+
+function waitforme(milisec) { 
+  return new Promise(resolve => { 
+      setTimeout(() => { resolve('') }, milisec); 
+  }) 
 }
 
 //generate array function
 function generateArray() {
   arr = [];
   sortingWindow.innerHTML = "";
-  determineSize = document.querySelector(".size-slider").value;
-  for (let i = 0; i < determineSize; i++) {
-    arr[i] = Math.random();
+  arraySize = document.querySelector(".size-slider").value;
+  for (let i = 0; i < arraySize; i++) {
+    // arr[i] = Math.random();
+    arr.push(Math.floor(Math.random()*100)+5);
   }
-  const bars = document.querySelector('#bars');
 
   // creating multiple div elements using loop and adding multiple class
-  for(let i=0; i<determineSize; ++i){
+  for(let i=0; i<arraySize; ++i){
       const bar = document.createElement("div");
-
-      bar.style.height = arr[i] * 100 + "%";
+      bar.style.height = arr[i] + "%";
       bar.classList.add('bar');
       bar.classList.add('bar-style');
       bar.classList.add(`barNo${i}`);
-      bars.appendChild(bar);    
+      sortingWindow.appendChild(bar);    
   }
 }
 
-//update array size function
-function updateArraySize() {
-  determineSize = document.querySelector(".size-slider").value;
-  generateArray();
-}
 //update speed function
 function updateSpeed() {
   determineSpeed = document.querySelector(".speed-slider").value;
@@ -57,20 +55,19 @@ function updateSpeed() {
       animationSpeed = 120;
       break;
     case "2":
-      animationSpeed = 80;
+      animationSpeed = 90;
       break;
     case "3":
-      animationSpeed = 50;
+      animationSpeed = 150;
       break;
     case "4":
-      animationSpeed = 30;
+      animationSpeed = 70;
       break;
     case "5":
-      animationSpeed = 10;
+      animationSpeed = 20;
       break;
   }
 }
-
 
 function disableButtons() {
   sortingButtons.forEach((button) => {
