8c9eee323df81dc61dd35849f2f313edea84826a
actuallyakshat <akshatdubey0808@gmail.com>
2023-10-31 17:14:26 +0530

    routine push

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -3,6 +3,10 @@ let thumb = document.querySelector(".toggle-thumb");
 let leftSection = document.querySelector(".left-section");
 let wrapper = document.querySelector(".wrapper");
 
+let timeBox = document.querySelector(".time");
+let spaceBox = document.querySelector(".space");
+
+
 slider.addEventListener("click", toggle);
 thumb.addEventListener("click", toggle);
 
@@ -11,4 +15,6 @@ function toggle() {
   wrapper.classList.toggle("dark-theme");
   leftSection.classList.toggle("left-section-dark");
   sortingWindow.classList.toggle("sorting-window-dark");
+  timeBox.classList.toggle("time-dark");
+  spaceBox.classList.toggle("space-dark");
 }
