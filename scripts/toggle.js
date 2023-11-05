c0871ca8b141c7a2badf70d4c3f8804cbbd29441
dhruvkaushik305 <dhruvkaushik305@gmail.com>
2023-11-05 08:13:23 +0530

    the toggle button made functional

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -1,5 +1,7 @@
 let slider = document.querySelector(".toggle-slider");
 let thumb = document.querySelector(".toggle-thumb");
+let sun = document.querySelector("span .sun-s");
+let moon = document.querySelector("span .moon-s");
 let leftSection = document.querySelector(".left-section");
 let wrapper = document.querySelector(".wrapper");
 let timeBox = document.querySelector(".time");
@@ -8,9 +10,10 @@ let spaceTop = document.querySelector(".space-top");
 let spaceBox = document.querySelector(".space");
 let tableCells = document.querySelectorAll(".cell");
 
-
 slider.addEventListener("click", toggle);
 thumb.addEventListener("click", toggle);
+sun.addEventListener("click", toggleSun);
+moon.addEventListener("click", toggleMoon);
 
 // Check the time and enable dark mode after 7 PM
 function checkIfNight() {
@@ -20,7 +23,16 @@ function checkIfNight() {
     toggle();
   }
 }
-
+function toggleSun() {
+  sun.style.display = "none";
+  moon.style.display = "inline-block";
+  toggle();
+}
+function toggleMoon() {
+  moon.style.display = "none";
+  sun.style.display = "inline-block";
+  toggle();
+}
 function toggle() {
   console.log("Hi");
   thumb.classList.toggle("toggle-on");
@@ -28,11 +40,9 @@ function toggle() {
   pauseStatus.classList.toggle("dark-theme");
   leftSection.classList.toggle("left-section-dark");
   sortingWindow.classList.toggle("sorting-window-dark");
-  // timeBox.classList.toggle("time-dark");
   timeTop.classList.toggle("time-top-dark");
-  // spaceBox.classList.toggle("space-dark");
   spaceTop.classList.toggle("space-top-dark");
-  tableCells.forEach(element => {
+  tableCells.forEach((element) => {
     element.classList.toggle("dark-borders");
   });
 }
